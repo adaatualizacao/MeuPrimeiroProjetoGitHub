8279537f121d882d132b820a4868947b89b408e2
João Carlos <joao.carlos@usereserva.com>
2022-05-06 22:14:40 -0300

    Outra funcao

--- AloGitHub/AloGitHub/Program.cs
+++ AloGitHub/AloGitHub/Program.cs
@@ -8,9 +8,18 @@ namespace AloGitHub
 {
     class Program
     {
-        static void EscreveNaTela()
+        static void OutraFuncao(String txt)
         {
             Console.WriteLine(txt);
+        }
+        static void EscreveNaTela(String txt)
+        {
+            Console.WriteLine(txt);
+        }
+
+        static void EscreveNaTela2(String txt)
+        {
+            EscreveNaTela(txt);
         }
         static void Main(string[] args)
         {
